ef95e0da46b03fe57c50d4aeca7d5b985404841a
cyy444243429-jpg <cyy444243429@gmail.com>
2025-11-17 01:35:35 +0800

    Update HudAmapDriveWayAdapter.kt

--- app/src/main/java/com/fkdeepal/tools/ext/adapter/HudAmapDriveWayAdapter.kt
+++ app/src/main/java/com/fkdeepal/tools/ext/adapter/HudAmapDriveWayAdapter.kt
@@ -10,51 +10,61 @@ import com.fkdeepal.tools.ext.bean.AmapDriveWayInfoBean
 import com.fkdeepal.tools.ext.databinding.ItemHubDriveWayBinding
 import com.fkdeepal.tools.ext.utils.AppUtils
 
-class HudAmapDriveWayAdapter( mData: ArrayList<AmapDriveWayInfoBean>) : BaseAdapter<ItemHubDriveWayBinding,AmapDriveWayInfoBean>(AppUtils.appContext,
-                                                                                                                                 mData) {
+class HudAmapDriveWayAdapter(mData: ArrayList<AmapDriveWayInfoBean>) : BaseAdapter<ItemHubDriveWayBinding, AmapDriveWayInfoBean>(
+    AppUtils.appContext,
+    mData
+) {
     private val mResources by lazy {
-       mContext. getResources()
+        mContext.resources
     }
-    private val mPackageName  by lazy {
-        mContext.getPackageName()
+    private val mPackageName by lazy {
+        mContext.packageName
     }
-    override fun onCreateViewBinding(layoutInflater: LayoutInflater,
-                                     parent: ViewGroup,
-                                     viewType: Int): ItemHubDriveWayBinding {
+
+    override fun onCreateViewBinding(
+        layoutInflater: LayoutInflater,
+        parent: ViewGroup,
+        viewType: Int
+    ): ItemHubDriveWayBinding {
         return ItemHubDriveWayBinding.inflate(layoutInflater, parent, false)
     }
 
-    fun setImageDrawable(viewBinding: ItemHubDriveWayBinding,resourceName:String,
-                         fail:()-> Unit){
+    fun setImageDrawable(viewBinding: ItemHubDriveWayBinding, resourceName: String,
+                         fail: () -> Unit) {
 
         runCatching {
-            val resourceId = mResources.getIdentifier(resourceName, "drawable", mPackageName);
-            var drawable = ContextCompat.getDrawable(mContext,resourceId)
-            if (drawable!=null){
+            // 修改：查找xml资源而不是png
+            val resourceId = mResources.getIdentifier(resourceName, "drawable", mPackageName)
+            val drawable = ContextCompat.getDrawable(mContext, resourceId)
+            if (drawable != null) {
                 viewBinding.ivIcon.setImageDrawable(drawable)
-            }else{
+            } else {
                 viewBinding.ivIcon.setImageDrawable(null)
+                fail.invoke()
             }
         }.onFailure {
             viewBinding.ivIcon.setImageDrawable(null)
             fail.invoke()
         }
     }
-    override fun setViewHolderData(viewBinding: ItemHubDriveWayBinding,
-                                   item: AmapDriveWayInfoBean,
-                                   position: Int) {
+
+    override fun setViewHolderData(
+        viewBinding: ItemHubDriveWayBinding,
+        item: AmapDriveWayInfoBean,
+        position: Int
+    ) {
         val icon = item.drive_way_lane_Back_icon
         viewBinding.tvValue.visibility = View.GONE
-        if (icon.isNullOrBlank()){
+        if (icon.isNullOrBlank()) {
+            // 修改：使用xml资源而不是png
             viewBinding.ivIcon.setImageResource(R.drawable.ic_land_89)
-        }else{
+        } else {
+            // 注意：资源名称不变，仍然是 "ic_land_xx"，但文件扩展名已改为.xml
             val resourceName = "ic_land_${item.drive_way_lane_Back_icon}"
-            setImageDrawable(viewBinding,resourceName,
-                             {
-                                 viewBinding.tvValue.visibility = View.VISIBLE
-                                 viewBinding.tvValue.setText(icon)
-                             })
+            setImageDrawable(viewBinding, resourceName) {
+                viewBinding.tvValue.visibility = View.VISIBLE
+                viewBinding.tvValue.text = icon
+            }
         }
-
     }
 }
