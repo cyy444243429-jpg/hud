0a6ac0497a5c5b32a7c061f130430d24d1452898
cyy444243429-jpg <cyy444243429@gmail.com>
2025-11-17 18:21:53 +0800

    Update AppApplication.kt

--- app/src/main/java/com/fkdeepal/tools/ext/app/AppApplication.kt
+++ app/src/main/java/com/fkdeepal/tools/ext/app/AppApplication.kt
@@ -1,6 +1,7 @@
 package com.fkdeepal.tools.ext.app
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import com.fkdeepal.tools.ext.timber.FileLoggingTree
 import com.fkdeepal.tools.ext.utils.AppUtils
 import com.fkdeepal.tools.ext.utils.FileUtils
@@ -9,6 +10,8 @@ import timber.log.Timber
 class AppApplication : Application(){
     override fun onCreate() {
         super.onCreate()
+        // 启用矢量图资源支持
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true)
         AppUtils.setContext(applicationContext)
         if (AppUtils.isDebug){
             Timber.plant(FileLoggingTree(FileUtils.getLogCacheFile(AppUtils.appContext)))
